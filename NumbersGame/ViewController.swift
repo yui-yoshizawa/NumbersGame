6a85982692f7a97f2da82dcb25d0ead70a5b9cb0
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-08 19:50:21 +0900

    入力値がInt型以外だった場合をエラーにする処理を追加

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -17,31 +17,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var numTextField: UITextField!
     
-    @IBOutlet weak var resultButton: UIButton!    // 消したら怒られた。
-    
     @IBOutlet weak var resultTextView: UITextView!
   
-    
     // 施行数
     var answerCount: Int = 0
     
-  
-   
     
-    // ---------------------------------------------------------------------------------------------------
-    // 【済】数値を入力して決定を行なったあと、答えの数と照らし合わせて「上か下か」判別がつくメッセージを表示させる。
-    // 【済】答えを当てた場合、再度ランダムな数値が割り当てられて継続して遊べること。
-    // 【済】入力内容のバリデーション（入力値チェック）を行う。（1〜100以外の入力をエラーとする）
-    // 【済】遊んだ履歴を「UITextView」を用いて表示する。
-    
-    
-    
-    
+    // ----------------------------------------------------------
     // Storyboard の情報を読み込み終わった後に呼ばれるやつ
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
     
     // この辺から関数とか書き始めればいいんじゃないかなぁ
     
@@ -50,64 +36,67 @@ class ViewController: UIViewController {
     var correctNum = Int.random(in: 1...100)    // 最初場所わかんなくていろいろ試したけど、ここで良いっぽい。多分。理解した。
     
     
-    
     // 【決定ボタン押した後の処理】をここに書くよ。
     // 回答をチェックする関数
     func checkAnswer() {
         
         // 1. TextField内の数値をInt型で取り出し、入力内容をチェック
         // 1-1. TextField内の数値をInt型で取り出す
-        let answerNum = Int(numTextField.text!)!    // Intつけないと、下の if answerNum < correctNum { で怒られる。var だと警告がでる。
-        
-        // 1-2. 入力内容をチェック 1~100以外の入力をエラーとする
-        if answerNum >= 1 && answerNum <= 100 {
+        if let answerNum = Int(numTextField.text!) {    // 入力値がInt型かどうか
+            
+            // 1-2. 入力内容をチェック 1~100以外の入力をエラーとする
+            if answerNum >= 1 && answerNum <= 100 {
+            } else {
+                // 1~100以外の時
+                errorAlert()    // エラー時のアラート呼出し
+                numTextField.text = ""    // 入力文字リセット。残ってたらうざいから。
+                return    // これがないと下の処理もしてしまう。例えば、[n回目] 答えは〜 っていうメッセージとか出てきちゃう。
+            }
+            
+            
+            // 2. ラベルを入力したテキストに変更する
+            numLabel.text = numTextField.text
+            
+            // numTextField に入力した文字列をリセット。次に入力する時、残ってたらうざいから。
+            numTextField.text = ""
+            
+            
+            // 3. 正解と照らし合わせて「上か下か」判別がつくメッセージを表示させる
+            if answerNum < correctNum {
+                // 3-1. 正解よりも低い場合
+                answerCount += 1    // 施行数を1増やす
+                showAlert(message: "答えは\(answerNum)より高い値です。")    // アラートを表示させたい → アラートの関数を下に作ってきた！
+                resultTextView.text = resultTextView.text + "\n[\(answerCount)回目] 答えは\(answerNum)より高い値です。"    // \n を "" の外に書いたら怒られた。
+                
+                
+            } else if answerNum > correctNum {
+                // 3-2. 正解よりも高い場合
+                answerCount += 1
+                showAlert(message: "答えは\(answerNum)より低い値です。")
+                resultTextView.text = resultTextView.text + "\n[\(answerCount)回目] 答えは\(answerNum)より低い値です。"
+                
+                
+            } else if answerNum == correctNum {
+                // 3-3. 正解と一致した場合
+                answerCount += 1
+                showAlert(message: "\(answerCount)回目に正解しました！\n数字をリセットしました！")    // \n で改行
+                resultTextView.text = resultTextView.text + "\n[正解] 答えは\(correctNum)でした。"
+                
+                // リセットしていくよ〜
+                numLabel.text = "??"
+                answerCount = 0
+                correctNum = Int.random(in: 1...100)    // できたああああああああああああ！！！！！
+                // correctNum を定数にしていたのが原因でエラーがでていた。変数にして解決。
+                
+                
+            } else {
+                // エラーをわかりやすくする
+                print("エラー")
+                return
+            }
+            
         } else {
-            // 1~100以外の時
-            errorAlert()    // エラー時のアラート呼出し
-            numTextField.text = ""    // 入力文字リセット。残ってたらうざいから。
-            return    // これがないと下の処理もしてしまう。例えば、[n回目] 答えは〜 っていうメッセージとか出てきちゃう。
-        }
-        
-        
-        // 2. ラベルを入力したテキストに変更する
-        numLabel.text = numTextField.text
-        
-        // numTextField に入力した文字列をリセット。次に入力する時、残ってたらうざいから。
-        numTextField.text = ""
-        
-        
-        // 3. 正解と照らし合わせて「上か下か」判別がつくメッセージを表示させる
-        if answerNum < correctNum {
-            // 3-1. 正解よりも低い場合
-            answerCount += 1    // 施行数を1増やす
-            showAlert(message: "答えは\(answerNum)より高い値です。")    // アラートを表示させたい → アラートの関数を下に作ってきた！
-            resultTextView.text = resultTextView.text + "\n[\(answerCount)回目] 答えは\(answerNum)より高い値です。"    // \n を "" の外に書いたら怒られた。
-            
-            
-        } else if answerNum > correctNum {
-            // 3-2. 正解よりも高い場合
-            answerCount += 1
-            showAlert(message: "答えは\(answerNum)より低い値です。")
-            resultTextView.text = resultTextView.text + "\n[\(answerCount)回目] 答えは\(answerNum)より低い値です。"
-            
-            
-        } else if answerNum == correctNum {
-            // 3-3. 正解と一致した場合
-            answerCount += 1
-            showAlert(message: "\(answerCount)回目に正解しました！\n数字をリセットしました！")    // \n で改行
-            resultTextView.text = resultTextView.text + "\n[正解] 答えは\(correctNum)でした。"
-            
-            // リセットしていくよ〜
-            numLabel.text = "??"
-            answerCount = 0
-            correctNum = Int.random(in: 1...100)    // できたああああああああああああ！！！！！
-                                                    // correctNum を定数にしていたのが原因でエラーがでていた。変数にして解決。
-            
-            
-        } else {
-            // エラーをわかりやすくする
-            print("エラー")
-            return
+            errorAlert()    // Int型ではなかった時。エラーを表示。
         }
     }
     
@@ -144,8 +133,6 @@ class ViewController: UIViewController {
         present(alert2, animated: true, completion: nil)
         
     }
-    
-    
     
     
     // Action 紐付け
